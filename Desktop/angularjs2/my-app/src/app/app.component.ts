be321b82749a279e4374c2457e63f93075fba11d
madhu9030 <msreddy490@gmail.com>
2017-01-09 19:12:04 -0600

    firstcommit

---
+++ Desktop/angularjs2/my-app/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  
+
+})
+
+
+export class AppComponent {
+  
+  users = [
+    { id: 25, name: 'madhu', username: 'sanaiahgari' },
+    { id: 26, name: 'sudab', username: 'aakepati' },
+    { id: 27, name: 'reddy', username: 'nooka' }
+  ];
+  activeUser;
+
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
+
+  
+}
+
